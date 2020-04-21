89c4c217b100be985b6edfc8b3e4f083691010a5
taro1121 <shintaro.eiyo@gmail.com>
2020-04-21 00:25:28 -0400

    img folder specification

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.static('.'));//'src'
 //make way for some custom css, js and images
 app.use('/css', express.static(__dirname + '/css'));//'/src/css'
 app.use('/js', express.static(__dirname + '/js'));//'/src.js'
-app.use('/images', express.static(__dirname + '/images'));//'/src/images'
+app.use('/images', express.static(__dirname + '/img'));//'/src/images'
 
 var server = app.listen(8081, function(){
     var port = server.address().port;
